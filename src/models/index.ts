41f8c7c1d5a741503db375db69505d5b29403977
alvar.dev <alvaropedrozo07@gmail.com>
2024-11-06 16:42:35 -0500

    feature: login page

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,4 @@
 export * from './routes.model'
 export * from './sidebarItems.model'
 export * from './invoicesItems.model'
+export * from './form.model'
--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -1,5 +1,24 @@
-export function Login(){
+import { Layout } from "@/layouts";
+import { Form } from "./components";
+import { Link } from "react-router-dom";
+
+export function Login() {
   return (
-    <h1>Login</h1>
+    <Layout title="SC - Login">
+      <main className="min-h-dvh flex flex-col items-center justify-center border-white/20">
+        <section className="border border-white/50 p-8 rounded-xl flex flex-col gap-4">
+          <header>
+            <h1 className="text-3xl font-bold">Login</h1>
+            <p className="text-zinc-400">Enter your email below to login to your account.</p>
+          </header>
+          <Form />
+          <footer>
+            <p className="text-sm flex items-center gap-2">
+              Don't have an account? <Link to="/register" className="text-zinc-400 hover:underline">Sign up</Link>
+            </p>
+          </footer>
+        </section>
+      </main>
+    </Layout>
   );
-};
+}
